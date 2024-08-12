459aac99c86cbe33407802b9e94ef0b0b11dbb57
MichnoM <115069540+MichnoM@users.noreply.github.com>
2024-08-12 22:19:47 +0200

    isaac_v0.0.2

--- pygame/game.py
+++ pygame/game.py
@@ -1,5 +1,6 @@
 import pygame
 import spritesheet
+import random
 
 pygame.init()
 
@@ -9,11 +10,13 @@ window = pygame.display.set_mode((window_width, window_height))
 
 pygame.display.set_caption("Game")
 
-isaacSprites = pygame.image.load('sprites/Isaac.png')
+isaac_sprites = pygame.image.load('sprites/Isaac.png')
+tear_sprites = pygame.image.load('sprites/tear.png')
 background = pygame.image.load('sprites/background.png')
 background = pygame.transform.scale(background, (window_width, window_height))
 
-sprite_sheet = spritesheet.SpriteSheet(isaacSprites)
+sprite_sheet = spritesheet.SpriteSheet(isaac_sprites)
+tear_spritesheet = spritesheet.SpriteSheet(tear_sprites)
 
 clock = pygame.time.Clock()
 last_update = pygame.time.get_ticks()
@@ -30,32 +33,29 @@ class Player(object):
         self.up = False
         self.down = False
         self.stationary = True
-        self.attack_speed = 1
-        self.cooldown_tracker = 0
-
-    def on_cooldown(self):
-        self.cooldown_tracker += clock.get_time()
-        if self.cooldown_tracker > 400/self.attack_speed:
-            self.cooldown_tracker = 0
-            return False
-        return True
+        self.attack_speed = 5
+        self.frame = 0
 
     def draw(self, window):
-        if self.left:
-            walk_left = sprite_sheet.get_image(2)
-            window.blit(walk_left, (self.cord_x, self.cord_y))
-        elif isaac.right:
-            walk_right = sprite_sheet.get_image(3)
-            window.blit(walk_right, (self.cord_x, self.cord_y))
-        elif self.up:
-            walk_up = sprite_sheet.get_image(1)
-            window.blit(walk_up, (self.cord_x, self.cord_y))
-        elif self.down:
-            walk_down = sprite_sheet.get_image(0)
-            window.blit(walk_down, (self.cord_x, self.cord_y))
-        elif self.stationary:
-            not_moving = sprite_sheet.get_image(0)
-            window.blit(not_moving, (self.cord_x, self.cord_y))
+        if not custom_sprite_event:
+            if self.left:
+                walk_left = sprite_sheet.get_image(2)
+                window.blit(walk_left, (self.cord_x, self.cord_y))
+            elif isaac.right:
+                walk_right = sprite_sheet.get_image(3)
+                window.blit(walk_right, (self.cord_x, self.cord_y))
+            elif self.up:
+                walk_up = sprite_sheet.get_image(1)
+                window.blit(walk_up, (self.cord_x, self.cord_y))
+            elif self.down:
+                walk_down = sprite_sheet.get_image(0)
+                window.blit(walk_down, (self.cord_x, self.cord_y))
+            elif self.stationary:
+                not_moving = sprite_sheet.get_image(0)
+                window.blit(not_moving, (self.cord_x, self.cord_y))
+        else:
+            custom = sprite_sheet.get_image(self.frame)
+            window.blit(custom, (self.cord_x, self.cord_y))
 
 class Projectile(object):
     def __init__(self, x, y, radius, colour, direction):
@@ -68,18 +68,69 @@ class Projectile(object):
 
     def draw(self, window):
         pygame.draw.circle(window, self.colour, (self.x, self.y), self.radius)
+        tear_sprite = tear_spritesheet.get_image(0, 130, 130, 0.2)
+        window.blit(tear_sprite, (self.x - round(tear_sprite.get_width()//2), self.y - round(tear_sprite.get_height()//2)))
+
+class Enemy(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        # self.end = end
+        self.vel_x = 3
+        self.vel_y = 3
+        # self.path = [self.x, self.end]
+        self.direction = 1
+        self.enemy_rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, window):
+        self.move()
+        pygame.draw.rect(window, (0, 255, 0), self.enemy_rect)
+
+    def move(self):
+        if self.enemy_rect.left <= 20 or self.enemy_rect.right >= 780:
+            self.direction *= -1
+            self.vel_x = random.randint(0, 8) * self.direction
+            self.vel_y = random.randint(0, 8) * self.direction
+            if self.vel_x == 0 and self.vel_y == 0:
+                self.vel_x = random.randint(2, 8) * self.direction
+                self.vel_y = random.randint(2, 8) * self.direction
+
+        if self.enemy_rect.top <= 20 or self.enemy_rect.bottom >= 580:
+            self.direction *= -1
+            self.vel_x = random.randint(0, 8) * self.direction
+            self.vel_y = random.randint(0, 8) * self.direction
+            if self.vel_x == 0 and self.vel_y == 0:
+                self.vel_x = random.randint(2, 8) * self.direction
+                self.vel_y = random.randint(2, 8) * self.direction
+
+        self.enemy_rect.left += self.vel_x
+        self.enemy_rect.top += self.vel_y
+            # if last_update - current_time > 2000:
+            #     self.direction *= -1
+            #     self.vel = random.randint(2,8) * self.direction
 
 def redrawGameWindow():
     window.blit(background, (0, 0))
     isaac.draw(window)
     for tear in tears:
         tear.draw(window)
+    enemy1.draw(window)
 
     pygame.display.update()
+
+on_cooldown_event = pygame.USEREVENT + 1
+custom_sprite_event = pygame.USEREVENT + 2
+
+on_cooldown = False
+custom_sprite_event = False
 
 #mainloop
 isaac = Player(372, 267, 56, 66)
 tears = []
+enemy1 = Enemy(100, 100, 50, 50)
+
 run = True
 while run:
     clock.tick(60)
@@ -90,6 +141,12 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == on_cooldown_event:
+            on_cooldown = False
+            pygame.time.set_timer(on_cooldown_event, 0)
+        if event.type == custom_sprite_event:
+            custom_sprite_event = False
+            pygame.time.set_timer(custom_sprite_event, 0)
 
     for tear in tears:
         if tear.x < window_width and tear.x > 0:
@@ -140,30 +197,54 @@ while run:
         isaac.left = False
         isaac.right = False
 
-    if keys[pygame.K_UP] and not isaac.on_cooldown():
-        tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 6, (0, 0, 0), "up"))
-        isaac.up = True
-        isaac.down = False
-        isaac.left = False
-        isaac.right = False
-    elif keys[pygame.K_DOWN] and not isaac.on_cooldown():
-        tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 6, (0, 0, 0), "down"))
-        isaac.down = True
-        isaac.up = False
-        isaac.left = False
-        isaac.right = False
-    elif keys[pygame.K_LEFT] and not isaac.on_cooldown():
-        tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 6, (0, 0, 0), "left"))
-        isaac.left = True
-        isaac.up = False
-        isaac.down = False
-        isaac.right = False
-    elif keys[pygame.K_RIGHT] and not isaac.on_cooldown():
-        tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 6, (0, 0, 0), "right"))
-        isaac.right = True
-        isaac.up = False
-        isaac.down = False
-        isaac.left = False
+    if keys[pygame.K_UP]:
+        custom_sprite_event = True
+        isaac.frame = 1
+        pygame.time.set_timer(custom_sprite_event, 200)
+        if not on_cooldown:
+            tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 10, (0,0,0), "up"))
+            isaac.up = True
+            isaac.down = False
+            isaac.left = False
+            isaac.right = False
+            on_cooldown = True
+            pygame.time.set_timer(on_cooldown_event, 1000//isaac.attack_speed)
+    elif keys[pygame.K_DOWN]:
+        custom_sprite_event = True
+        isaac.frame = 0
+        pygame.time.set_timer(custom_sprite_event, 200)
+        if not on_cooldown:
+            tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 10, (0,0,0), "down"))
+            isaac.down = True
+            isaac.up = False
+            isaac.left = False
+            isaac.right = False
+            on_cooldown = True
+            pygame.time.set_timer(on_cooldown_event, 1000//isaac.attack_speed)
+    elif keys[pygame.K_LEFT]:
+        custom_sprite_event = True
+        isaac.frame = 2
+        pygame.time.set_timer(custom_sprite_event, 200)
+        if not on_cooldown:
+            tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 10, (0,0,0), "left"))
+            isaac.left = True
+            isaac.up = False
+            isaac.down = False
+            isaac.right = False
+            on_cooldown = True
+            pygame.time.set_timer(on_cooldown_event, 1000//isaac.attack_speed)
+    elif keys[pygame.K_RIGHT]:
+        custom_sprite_event = True
+        isaac.frame = 3
+        pygame.time.set_timer(custom_sprite_event, 200)
+        if not on_cooldown:
+            tears.append(Projectile(round(isaac.cord_x + isaac.width//2), round(isaac.cord_y + isaac.height//2), 10, (0,0,0), "right"))
+            isaac.right = True
+            isaac.up = False
+            isaac.down = False
+            isaac.left = False
+            on_cooldown = True
+            pygame.time.set_timer(on_cooldown_event, 1000//isaac.attack_speed)
 
     redrawGameWindow()
 
