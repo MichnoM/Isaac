bf928c09a1f515cd5336f2e9665209bc70bc4e02
MichnoM <115069540+MichnoM@users.noreply.github.com>
2024-08-12 03:47:17 +0200

    Isaac_v0.0.1

---
+++ pygame/spritesheet.py
@@ -0,0 +1,13 @@
+import pygame
+
+class SpriteSheet():
+    def __init__(self, image):
+        self.sheet = image
+
+    def get_image(self, frame, width = 56, height = 66, scale = 1, colour = (0, 0, 0)):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
+        image = pygame.transform.scale(image, (width * scale, height * scale))
+        image.set_colorkey(colour)
+    
+        return image
